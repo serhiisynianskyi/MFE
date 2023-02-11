a2c95159bedb144c1a50c2c20d51795368b0d4f6
Serhii <serhii.synianskyi@pragmaticplay.com>
2023-02-11 18:06:11 +0200

    test commit

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there! Test</h1>
+      <h1>Hi there! Tes</h1>
       <hr />
       <MarketingApp />
     </div>
