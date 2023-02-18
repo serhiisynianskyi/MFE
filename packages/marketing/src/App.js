53a32cd6110489fb6234fe8fe6891faaddab5fbb
Serhii <serhii.synianskyi@pragmaticplay.com>
2023-02-18 18:14:10 +0200

    fix(marketing.yml): WIP

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -8,7 +8,7 @@ import Pricing from './components/Pricing';
 export default () => {
   return (
     <div>
-      <p>Marketing test!</p>
+      <p>Marketing test</p>
       <StylesProvider>
         <BrowserRouter>
           <Switch>
