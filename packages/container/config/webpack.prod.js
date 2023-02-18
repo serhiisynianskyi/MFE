06e6fa3154fd0f9ed1516e65d9c365a1581492cc
Serhii <serhii.synianskyi@pragmaticplay.com>
2023-02-18 18:22:18 +0200

    fix(container): WIP

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,7 +4,7 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const domain1 = process.env.PRODUCTION_DOMAIN;
-const domain = 'https://ddfcdpxt35y1w.cloudfront.net/';
+const domain = 'https://ddfcdpxt35y1w.cloudfront.net';
 
 const prodConfig = {
   mode: 'production',
